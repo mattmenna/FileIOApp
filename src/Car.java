aa19b2bdeedbcb1d918a0c77b24092e02a508ada
Matthew Menna <mattmenna@gmail.com>
2017-07-11 10:45:14 -0400

    Initial Commit

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,41 @@
+import java.io.Serializable;
+
 /**
  * Created by Matt on 7/11/2017.
  */
-public class Car {
-    //POJO - plain old java object
-    //      no argument constructor
-    //      getter and setters for all private variables
+public class Car implements Serializable{
+    /* POJO - plain old java object
+        no argument constructor
+        getter and setters for all private variables
+        Implement serializable interface
+
+      The serializable interface is a tagging interface that notifies
+       an application that objects can be serialized (aka flattened)
+       Typically use serializable for files that only my java app will use
+
+    */
+    transient int test;  // this value doesn't get saved because its transient
+    private int mileage;
+    private String make;
+
+    @Override
+    public String toString() {
+        return make + " " + mileage;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
 }
--- src/FileIOApp.java
+++ src/FileIOApp.java
@@ -2,4 +2,11 @@
  * Created by Matt on 7/11/2017.
  */
 public class FileIOApp {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.setMake("Mercedes");
+        car.setMileage(5);
+        System.out.println(car.toString());
+    }
 }
