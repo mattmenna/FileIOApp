712be211c672b54626a871919e7ebdd66c050199
Matthew Menna <mattmenna@gmail.com>
2017-07-11 10:32:55 -0400

    Initial Commit

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Matt on 7/11/2017.
+ */
+public class Car {
+    //POJO - plain old java object
+    //      no argument constructor
+    //      getter and setters for all private variables
+}
