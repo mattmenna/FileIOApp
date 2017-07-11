f6df937dc1ef38bf6593bc9f514c8363e7b5586e
Matthew Menna <mattmenna@gmail.com>
2017-07-11 12:00:28 -0400

    Buffered reader

--- src/TextWriterDemo.java
+++ src/TextWriterDemo.java
@@ -1,8 +1,6 @@
 import com.sun.xml.internal.fastinfoset.util.StringArray;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by Matt on 7/11/2017.
@@ -29,9 +27,9 @@ public class TextWriterDemo {
 
         //list all contents in a directory (similiar to ls command line)
 
-        if(dir.isDirectory()) {
+        if (dir.isDirectory()) {
             String[] dirContents = dir.list();
-            for (int i = 0; i <dirContents.length ; i++) {
+            for (int i = 0; i < dirContents.length; i++) {
                 System.out.println(dirContents[i]);
             }
         }
@@ -41,6 +39,29 @@ public class TextWriterDemo {
         //delete a file returns true if succesful
         boolean isDeleted = f.delete();
         System.out.println(isDeleted);
+
+        // read from file
+        try {
+            File myFile = new File("test.txt");
+            // FileReader is a connection stream for characters that connects to a text file
+
+            FileReader reader = new FileReader(myFile);
+
+            //BufferedReader allows the File Reader to chain to it
+            //it only goes back to check the file once the buffer is empty
+            BufferedReader buff = new BufferedReader(reader);
+            //declare a string variable to hold each line as it's read from the file
+            String line = null;
+
+            //this while loop says read a line for text then assign it to the string line
+            //while that variable is not null keep printing lines
+            while ((line = buff.readLine()) != null) { // changed catch to General Exception
+                System.out.println(line);
+            }
+            buff.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     } // end main
